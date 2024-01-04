b769697db703ce6989abd61661725aa0e5fac4c1
Georgefwm <georgemclachlan2@gmail.com>
2024-01-05 12:11:28 +1300

    Add kanagawa colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,12 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.colorscheme.kanagawa" },
+  -- { -- further customize the options set by the community
+  --   "kanagawa",
+  --   opts = {
+  --     
+  --   },
+  -- },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- plugins/core.lua
+++ plugins/core.lua
@@ -20,6 +20,7 @@ return {
       return opts
     end,
   },
+  require("user.plugins.kanagawa"),
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
